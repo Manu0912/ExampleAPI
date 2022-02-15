bc27211b973f261e72efc6d80c6b9dd56e418c27
Emanuel Hugo Cacheda <emanuelcacheda@hotmail.com>
2022-02-15 16:54:12 -0700

    first commit

---
+++ ExampleAPI/Models/ExampleContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ExampleAPI.Models
+{
+    public class ExampleContext : DbContext
+    {
+        public ExampleContext(DbContextOptions<ExampleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ExampleContext> TodoItems { get; set; } = null!;
+    }
+}
---
+++ ExampleAPI/Models/ExampleItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ExampleAPI.Models
+{
+    public class ExampleItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
