2eced28448ff529358f168cedc4e70efb7c40b06
Emanuel Hugo Cacheda <emanuelcacheda@hotmail.com>
2022-02-23 16:17:22 -0700

    new test

--- ExampleAPI/Models/ExampleItem.cs
+++ ExampleAPI/Models/ExampleItem.cs
@@ -9,27 +9,5 @@ namespace ExampleAPI.Models
         public bool IsCompleted { get; set; }
     }
 
-    public class ExampleItemValidator : AbstractValidator<ExampleItem>
-    {
-
-        public ExampleItemValidator()
-        {
-            RuleFor(exampleItem => exampleItem.Name)
-                .MaximumLength(50).WithMessage("Name cannot be longer than 50 letters")
-                .MinimumLength(2).WithMessage("Name has to be longer than 2 letters")
-                .NotEmpty().WithMessage("Name cannot be empty")
-                .NotNull().WithMessage("Name cannot be null");
-
-            RuleFor(exampleItem => exampleItem.Id)
-                .GreaterThan(0).WithMessage("Id has to be greater than zero")
-                .NotEmpty().WithMessage("Id cannot be empty")
-                .NotNull().WithMessage("Id cannot be null");
-
-            RuleFor(exampleItem => exampleItem.IsCompleted)
-                .NotNull().WithMessage("Name cannot be null");
-        
-        }
-
-    }
 }
 
--- ExampleTests/ApiTest.cs
+++ ExampleTests/ApiTest.cs
@@ -7,34 +7,49 @@ namespace ExampleTests
 {
     public class ApiTest
     {
-        private readonly ExampleItemValidator _validator = new ExampleItemValidator();
+        private readonly ExampleItemValidator _validator = new();
 
         [Fact]
-        public void TestNameValidatorPass()
+        public void TestNameValidator_Pass()
         {
             var result = _validator.TestValidate(new ExampleItem() { Id = 1, Name = "sjdfbdskj", IsCompleted = true });
             result.ShouldNotHaveValidationErrorFor(item => item.Name);
         }
 
         [Fact]
-        public void TestNameValidatorFail()
+        public void TestNameValidator_IsLongerThanMaxLength_Fail()
         {
             var result = _validator.TestValidate(new ExampleItem() { Id = 1, Name = "sjdfbdskjsjdfbdskjsjdfbdskjsjdfbdskjsjdfbdskjsjdfbdskjsjdfbdskjsjdfbdskjsjdfbdskjsjdfbdskjsjdfbdskjsjdfbdskjsjdfbdskj", IsCompleted = true });
             result.ShouldHaveValidationErrorFor(item => item.Name);
         }
 
         [Fact]
-        public void TestIdValidatorPass()
+        public void TestNameValidator_IsNull_Fail()
+        {
+            var result = _validator.TestValidate(new ExampleItem() { Id = 1, Name = null, IsCompleted = true });
+            result.ShouldHaveValidationErrorFor(item => item.Name);
+        }
+
+        [Fact]
+        public void TestNameValidator_IsEmpty_Fail()
+        {
+            var result = _validator.TestValidate(new ExampleItem() { Id = 1, Name = "", IsCompleted = true });
+            result.ShouldHaveValidationErrorFor(item => item.Name);
+        }
+
+        [Fact]
+        public void TestIdValidator_Pass()
         {
             var result = _validator.TestValidate(new ExampleItem() { Id = 1, Name = "sjdfbdskj", IsCompleted = true });
             result.ShouldNotHaveValidationErrorFor(item => item.Id);
         }
 
         [Fact]
-        public void TestIdValidatorFail()
+        public void TestIdValidator_IsLessThanZero_Fail()
         {
             var result = _validator.TestValidate(new ExampleItem() { Id = -1, Name = "sjdfbdskj", IsCompleted = true });
-            result.ShouldHaveValidationErrorFor(item => item.Id);
+            result.ShouldNotHaveValidationErrorFor(item => item.Id);
         }
+
     }
 }
