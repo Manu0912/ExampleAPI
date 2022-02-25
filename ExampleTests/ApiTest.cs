67cf96327b2cb8681764a464b567f2f57723d54e
Emanuel Hugo Cacheda <emanuelcacheda@hotmail.com>
2022-02-25 10:42:48 -0700

    Test post method

--- ExampleTests/ApiTest.cs
+++ ExampleTests/ApiTest.cs
@@ -1,18 +1,21 @@
 ﻿using System.Net;
+using System.Net.Http.Json;
+using System.Reflection;
 using System.Threading.Tasks;
 using ExampleAPI.Models;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Xunit;
 
-namespace TodoDapper.Tests;
+namespace ExampleTests.ApiTest;
 
 public class SmokeTests
 {
     [Fact]
-    public async Task GetTodos_Returns_OK()
+    public async Task GetAll_Returns_OK()
     {
         await using var application = new TodoApplication();
 
@@ -20,6 +23,26 @@ public class SmokeTests
         var response = await client.GetAsync("/api/ExampleItems");
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Post_Returns_OK()
+    {
+        await using var application = new TodoApplication();
+        var client = application.CreateClient();
+        var response = await client.PostAsJsonAsync("/api/ExampleItems", new ExampleItem() { IsCompleted =true, Name = "aaaaa"});
+        
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Post_Returns_Error()
+    {
+        await using var application = new TodoApplication();
+        var client = application.CreateClient();
+        var response = await client.PostAsJsonAsync("/api/ExampleItems", new ExampleItem() { Name = "aaaaa" });
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 
 
@@ -33,6 +56,15 @@ public class SmokeTests
                 services.AddDbContext<TestContext>(options =>
                 {
                     options.UseInMemoryDatabase("InMemoryExampleTest");
+                });
+                services.AddControllers()
+                .AddFluentValidation(options =>
+                {
+                    // Validate child properties and root collection elements
+                    options.ImplicitlyValidateChildProperties = true;
+                    options.ImplicitlyValidateRootCollectionElements = true;
+                    // Automatic registration of validators in assembly
+                    options.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
                 });
             });
 
